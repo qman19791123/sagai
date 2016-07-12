66b8c47bf3a2afb201a8efa1d652a3be0aaa3b98
qman19791123 <kuren@163.com>
2016-07-12 17:59:54 +0800

    这个是 manulife 开发中额版本

---
+++ manulife/manulifeJump/userManage/xmldb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace userManage
+{
+    public class xmldb
+    {
+        public List<lists> Xmldata()
+        {
+
+            string path = "Data.xml";
+            List<lists> TList = new List<lists>();
+            XDocument doc = XDocument.Load(path);  
+            var query = (from item in doc.Element("xml").Elements()
+                         select new
+                         {
+                             TypeName = item.Value,
+                         }).ToList();
+            foreach (var item in query)
+            {
+                TList.Add(new lists { Content = item.TypeName.ToString() });
+            }
+            return TList;
+        }
+        public class lists
+        {
+            private string content;
+
+            public string Content
+            {
+                get { return content; }
+                set { content = value; }
+            }
+          
+
+        }
+    }
+}
